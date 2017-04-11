47b66d8cc6f142fe27c8562265e3928b38933e60
Rickard Sjöström <sjostric@gmail.com>
2017-04-11 20:05:37 +0200

    Initial commit

---
+++ src/main/java/com/sjostric/tdd/Person.java
@@ -0,0 +1,7 @@
+package com.sjostric.tdd;
+
+/**
+ * Created by rickard on 2017-04-11.
+ */
+public class Person {
+}
---
+++ src/test/java/com/sjostric/tdd/PersonTest.java
@@ -0,0 +1,8 @@
+package com.sjostric.tdd;
+
+/**
+ * Created by rickard on 2017-04-11.
+ */
+public class PersonTest {
+
+}
